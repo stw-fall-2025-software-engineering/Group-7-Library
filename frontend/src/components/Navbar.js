73093e66defae23b2bd3cbb328cb44270ef610eb
bomcnea1219 <bobbymcneal13@gmail.com>
2025-12-08 16:35:01 -0600

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,35 +1,18 @@
-// src/components/Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
-import "../App.css"; // Make sure CSS is imported
+import "../App.css";
 
 export default function Navbar() {
   return (
     <nav>
       <ul>
-        {/* Logo / Title */}
-        <li>
-          <Link to="/" className="logo">
-            Library System
-          </Link>
-        </li>
-
-        {/* Navigation Links */}
-        <li>
-          <Link to="/availability">Book Availability</Link>
-        </li>
-        <li>
-          <Link to="/waitlist">Waitlist</Link>
-        </li>
-        <li>
-          <Link to="/notifications">Notifications</Link>
-        </li>
-        <li>
-          <Link to="/feedback">Feedback</Link>
-        </li>
-        <li>
-          <Link to="/help">Help</Link>
-        </li>
+        <li><Link to="/" className="logo">Library System</Link></li>
+        <li><Link to="/availability">Book Availability</Link></li>
+        <li><Link to="/waitlist">Waitlist</Link></li>
+        <li><Link to="/return">Return Book</Link></li> {/* New */}
+        <li><Link to="/notifications">Notifications</Link></li>
+        <li><Link to="/feedback">Feedback</Link></li>
+        <li><Link to="/help">Help</Link></li>
       </ul>
     </nav>
   );
