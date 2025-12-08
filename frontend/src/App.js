937cc2c1fdc31da368794796727149e215bf51aa
bomcnea1219 <bobbymcneal13@gmail.com>
2025-12-08 16:35:48 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-// Import all pages
 import BookAvailability from "./pages/BookAvailability";
 import ReserveBook from "./pages/ReserveBook";
-import WaitlistStatus from "./pages/WaitlistStatus"; // Waitlist form page
+import WaitlistStatus from "./pages/WaitlistStatus";
+import ReturnBook from "./pages/ReturnBook";
 import Feedback from "./pages/Feedback";
 import Help from "./pages/Help";
 
@@ -19,7 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<BookAvailability />} />
             <Route path="/reserve" element={<ReserveBook />} />
-            <Route path="/waitlist" element={<WaitlistStatus />} /> {/* ← Waitlist page */}
+            <Route path="/waitlist" element={<WaitlistStatus />} />
+            <Route path="/return" element={<ReturnBook />} /> {/* New */}
             <Route path="/feedback" element={<Feedback />} />
             <Route path="/help" element={<Help />} />
           </Routes>
